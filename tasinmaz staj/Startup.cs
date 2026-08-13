af462a0a6b9787f4bcf0431d3c8a5d3de7f49ce2
bugrakesli <omerbugraakesli@gmail.com>
2026-08-13 14:59:17 +0300

    cleanup on backend

--- tasinmaz staj/Startup.cs
+++ tasinmaz staj/Startup.cs
@@ -15,8 +15,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TasinmazStaj.Interfaces;
-using TasinmazStaj.Services;
 
 namespace tasinmaz_staj
 {
