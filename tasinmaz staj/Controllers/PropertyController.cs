3197609e528305a9644001c6084eee73e5c02276
bugrakesli <omerbugraakesli@gmail.com>
2026-08-20 16:28:31 +0300

    Add using Microsoft.Extensions.Logging to PropertyController

--- tasinmaz staj/Controllers/PropertyController.cs
+++ tasinmaz staj/Controllers/PropertyController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 [ApiController]
 [Route("api/[controller]")]
